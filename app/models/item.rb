052904a8c2c0ccbe608e32a5606e4c77e09e3663
yutamiura7 <yuta6112miura@icloud.com>
2021-08-16 15:43:31 +0900

    itemモデルにバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,14 @@ class Item < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :itemname, presence: true
+  validates :text, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :load_id, presence: true
+  validates :area_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :price, presence: true
 end
