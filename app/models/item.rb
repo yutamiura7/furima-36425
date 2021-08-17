ccb1d3630f396b3fb9016fe64d0f8f7e73568dfb
yutamiura7 <yuta6112miura@icloud.com>
2021-08-17 20:57:37 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,13 +10,19 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :shippingday
 
-  validates :image, presence: true
-  validates :itemname, presence: true
-  validates :text, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :load_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shippingday_id, numericality: { other_than: 1, message: "can't be blank" }
+  with_options presence: true do
+    validates :image
+    validates :itemname
+    validates :text
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :load_id
+    validates :area_id
+    validates :shippingday_id
+  end
+
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
