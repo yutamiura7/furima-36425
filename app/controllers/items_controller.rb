036e31548ebe273e2f2824249a9d9ecf2fd1df54
yutamiura7 <yuta6112miura@icloud.com>
2021-08-19 15:32:01 +0900

    コントローラーとビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :move_to_new, except: [:index, :new, :show]
-  before_action :move_to_index, except: [:index, :new, :show]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_index, except: [:index, :new, :create, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -22,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -45,12 +41,6 @@ class ItemsController < ApplicationController
                                  :price).merge(user_id: current_user.id)
   end
 
-  def move_to_new
-    unless user_signed_in?
-      redirect_to action: :new
-    end
-  end
-
   def move_to_index
     item = Item.find(params[:id])
     unless current_user.id == item.user_id
@@ -58,4 +48,7 @@ class ItemsController < ApplicationController
     end
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
