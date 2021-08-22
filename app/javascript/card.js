530a7203591d3cabf31a1e559e85f7682a1f2b24
yutamiura7 <yuta6112miura@icloud.com>
2021-08-22 16:42:17 +0900

    デバッグ用の記述を削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,7 +13,6 @@ const pay = () => {
       exp_month: formData.get("buy_shippingaddress[card_exp_month]"),
       exp_year: `20${formData.get("buy_shippingaddress[card_exp_year]")}`,
     };
-    console.log(card)
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
